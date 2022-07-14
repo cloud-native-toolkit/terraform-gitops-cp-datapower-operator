8682dbd7e939f45117f8ce75e7994491dfa4c39f
Gowdhaman J <gowdhaman_j@yahoo.com>
2022-07-14 10:54:44 +0530

    Updated for CP4i 2022.2.1 (#8)
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- test/stages/stage2-datapower-operator.tf
+++ test/stages/stage2-datapower-operator.tf
@@ -1,14 +1,9 @@
-module "datapower-operator" {
+module "gitops_module" {
   source = "./module"
-
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
-  catalog = module.cp_catalogs.catalog_ibmoperators
-  kubeseal_cert = module.gitops.sealed_secrets_cert
-  entitlement_key = module.cp_catalogs.entitlement_key
-
+  
   # Pulling variables from CP4I dependency management
   channel  = module.cp4i-dependencies.datapower.channel
 
